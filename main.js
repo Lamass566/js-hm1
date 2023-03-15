63ed7f1e596441ce9d87822269cda371205ba2ba
Misha sysoiev <vsisoev@ukr.net>
2023-03-15 18:22:23 +0200

    f res

---
+++ main.js
@@ -0,0 +1,25 @@
+//1
+
+const NAME = "Misha";
+
+let admin = NAME;
+alert(admin);
+
+
+//2
+
+let days = 5;
+
+let hours = 24; //120
+let min = 60;
+let sec = 60;
+
+let sum = (hours * days) * min * sec;
+
+alert(sum)
+
+
+//3
+
+let userAnswer = prompt("ur name", "write here")
+alert(userAnswer);
